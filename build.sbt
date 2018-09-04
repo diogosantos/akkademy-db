e6f87930d1671ab8d70ce8ffaa9401f93e8441ee
Diogo Santos <diogo@diogosantos.com>
2018-09-04 12:13:37 +0200

    making it a standalone app

--- build.sbt
+++ build.sbt
@@ -4,8 +4,11 @@ version := "0.1"
 
 scalaVersion := "2.11.12"
 
+val akkaVersion = "2.5.16"
+
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.5.16",
-  "com.typesafe.akka" %% "akka-testkit" % "2.5.16" % "test",
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
   "org.scalatest" %% "scalatest" % "2.1.6" % "test"
 )
--- src/main/scala/com/akkademy/AkkademyDb.scala
+++ src/main/scala/com/akkademy/AkkademyDb.scala
@@ -1,8 +1,8 @@
 package com.akkademy
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorSystem, Props, Status}
 import akka.event.Logging
-import com.akkademy.messages.SetRequest
+import com.akkademy.messages.{GetRequest, KeyNotFoundException, SetRequest}
 
 import scala.collection.mutable
 
@@ -12,12 +12,25 @@ class AkkademyDb extends Actor {
 
   val logger = Logging(context.system, this)
 
-  override def receive = {
+  override def receive: PartialFunction[Any, Unit] = {
     case SetRequest(key, value) => {
       logger.info("Received SetRequest - key: {} value: {}", key, value)
       map.put(key, value)
+      sender() ! Status.Success()
     }
-    case o => logger.info("Received unknown object {}", o)
+    case GetRequest(key) => {
+      logger.info("Received GetRequest - key: {}", key)
+      map.get(key) match {
+        case Some(v) => sender() ! Status.Success(v)
+        case None => Status.Failure(KeyNotFoundException(key))
+      }
+    }
+    case _ => Status.Failure(new ClassNotFoundException())
   }
 
+}
+
+object Main extends App {
+  val actorSystem = ActorSystem("akkademy")
+  val akkademyDb = actorSystem.actorOf(Props[AkkademyDb], name = "akkademy-db")
 }
